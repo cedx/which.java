2c75b2437b5636b073681b5f90f8f36f604b34f3
Cédric Belin <cedric@belin.io>
2023-07-19 13:19:47 +0200

    Update the build system

--- scripts/Test.hx
+++ scripts/Test.hx
@@ -1,10 +1,11 @@
+using StringTools;
+
 /** Runs the test suite. **/
 function main() {
+	final pkg = "io.belin.which";
 	Sys.command("lix Build --debug");
-
-	final pattern = "io/belin/which/*.java";
 	Tools.setClassPath();
-	Sys.command('javac -d bin -g -Xlint:all,-processing test/$pattern');
+	Sys.command('javac -d bin -g -Xlint:all,-processing test/${pkg.replace(".", "/")}/*.java');
 
 	final exitCode = Sys.command("java org.junit.platform.console.ConsoleLauncher --select-package=io.belin.which");
 	if (exitCode != 0) Sys.exit(exitCode);
