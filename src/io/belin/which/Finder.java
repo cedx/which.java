dd888f961f924c7ad0a917b32d5a887884c56323
Cédric Belin <cedric@belin.io>
2023-07-21 23:57:53 +0200

    Code optimization

--- src/io/belin/which/Finder.java
+++ src/io/belin/which/Finder.java
@@ -20,7 +20,7 @@ final class Finder {
 	 * Value indicating whether the current platform is Windows.
 	 */
 	public static final boolean isWindows =
-		System.getProperty("os.name").startsWith("Windows") || List.of("cygwin", "msys").contains(Objects.requireNonNullElse(System.getenv("OSTYPE"), ""));
+		System.getProperty("os.name").startsWith("Windows") || List.of("cygwin", "msys").contains(Objects.toString(System.getenv("OSTYPE"), ""));
 
 	/**
 	 * The list of executable file extensions.
